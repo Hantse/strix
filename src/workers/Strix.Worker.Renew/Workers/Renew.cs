a4a6b5d576df679f2b4d27be2032812e672742e1
Patrick Gasseels <grasseelsp@hotmail.com>
2020-01-14 16:36:46 +0100

    Initial Commit

---
+++ src/workers/Strix.Worker.Renew/Workers/Renew.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Strix.Worker.Renew.Workers
+{
+    public class Renew : BackgroundService
+    {
+        private readonly ILogger<Renew> _logger;
+
+        public Renew(ILogger<Renew> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
